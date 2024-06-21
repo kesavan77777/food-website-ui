2140906566cdb2b72e48bcaefb97b10f876a8360
kesavan77777 <128518491+kesavan77777@users.noreply.github.com>
2024-06-21 08:48:39 +0530

    Update Exploremenu.jsx

--- src/components/Exploremenu/Exploremenu.jsx
+++ src/components/Exploremenu/Exploremenu.jsx
@@ -6,7 +6,7 @@ const Exploremenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore our menu</h1>
-        <p className='explore-menu-text' > menu is a fundamental element in both digital and physical environments, serving as a guide to available options and features. In the context of a restaurant, a menu provides a curated list of dishes and beverages,</p>
+        <p className='explore-menu-text' > menu is a fundamental element in both digital and physical environments, serving as a guide to available options and features. In the context of a restaurant.first when u visited a restaurant the first thing you would see is the menu</p>
         <div className="explore-menu-list">
             {menu_list.map((item,index)=>{
                 return(
